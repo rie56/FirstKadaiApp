c74e14e5a746f0e52685b5eec8820e1e8febeda8
rie.tomita.2 <techpractice2021@gmail.com>
2021-01-09 15:37:41 +0530

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Rie Tomita on 2021/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
